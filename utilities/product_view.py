5347dbc068e583e80ce9522a57d642b08c690a02
Jamexxu <jamesmrrcado2209@gmaail.com>
2025-05-19 16:20:09 +0800

    added single product view feature

--- utilities/product_view.py
+++ utilities/product_view.py
@@ -1,2 +1,15 @@
+from flask import render_template, request
+import os
+import json
+
+BOOKS_FILE = os.path.join(os.path.dirname(__file__), '..', 'data', 'books.json')
+
+def load_books():
+    with open(BOOKS_FILE, 'r') as f:
+        return json.load(f)
+
 def get_product_view():
-    pass
+    # For now, just show the first book as an example
+    books = load_books()
+    product = books[0] if books else {}
+    return render_template('product_view.html', product=product)
